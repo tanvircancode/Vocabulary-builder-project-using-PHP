7ce56beac7cc430ca43228fb64997348d90cb778
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-07-13 23:31:09 +0600

    updated

---
+++ tasks.php
@@ -0,0 +1,17 @@
+<?php
+
+$action = $_POST['action'];
+
+if($action == "register"){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    if($email && $password){
+
+    }
+    else
+    {
+        // email or pass missing
+    }
+
+
+}
